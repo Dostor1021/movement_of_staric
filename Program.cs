db8c23b9bf63384f64e6fe532b47b548a530f50a
Dostor1021 <152768388+Dostor1021@users.noreply.github.com>
2024-07-13 15:39:04 +0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int x = 0, y = 0; 
+
+        Console.CursorVisible = false; 
+
+        
+        steric(x, y);
+
+        while (true)
+        {
+            ConsoleKey key = Console.ReadKey().Key;
+
+
+            removeAsteric(x, y);
+
+
+            if (key == ConsoleKey.NumPad6)
+            {
+                x++;
+            }
+            else if (key == ConsoleKey.NumPad4)
+            {
+                x--;
+            }
+            else if (key == ConsoleKey.NumPad2)
+            {
+                y++;
+            }
+            else if (key == ConsoleKey.NumPad8)
+            {
+                y--;
+            }
+
+
+            steric(x, y);
+        }
+    }
+
+    static void steric(int x, int y)
+    {
+        Console.SetCursorPosition(x, y);
+        Console.Write("*");
+    }
+
+    static void removeAsteric(int x, int y)
+    {
+        Console.SetCursorPosition(x, y);
+        Console.Write(" ");
+    }
+}
